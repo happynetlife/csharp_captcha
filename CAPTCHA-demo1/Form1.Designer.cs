3e57ea423a7a924a755c43f40ec5f45c84d19746
happynetlife <happynetlife@qq.com>
2016-01-26 15:59:43 +0800

    版本1

---
+++ CAPTCHA-demo1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CAPTCHA_demo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picVcode = new System.Windows.Forms.PictureBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnRefresh1 = new System.Windows.Forms.Button();
+            this.picVcode1 = new System.Windows.Forms.PictureBox();
+            this.btnRefresh2 = new System.Windows.Forms.Button();
+            this.picVcode2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picVcode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVcode1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVcode2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picVcode
+            // 
+            this.picVcode.BackColor = System.Drawing.Color.White;
+            this.picVcode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picVcode.Location = new System.Drawing.Point(120, 12);
+            this.picVcode.Name = "picVcode";
+            this.picVcode.Size = new System.Drawing.Size(139, 50);
+            this.picVcode.TabIndex = 0;
+            this.picVcode.TabStop = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(276, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(104, 50);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "刷新";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnRefresh1
+            // 
+            this.btnRefresh1.Location = new System.Drawing.Point(276, 105);
+            this.btnRefresh1.Name = "btnRefresh1";
+            this.btnRefresh1.Size = new System.Drawing.Size(104, 50);
+            this.btnRefresh1.TabIndex = 3;
+            this.btnRefresh1.Text = "刷新";
+            this.btnRefresh1.UseVisualStyleBackColor = true;
+            this.btnRefresh1.Click += new System.EventHandler(this.btnRefresh1_Click);
+            // 
+            // picVcode1
+            // 
+            this.picVcode1.BackColor = System.Drawing.Color.White;
+            this.picVcode1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picVcode1.Location = new System.Drawing.Point(120, 105);
+            this.picVcode1.Name = "picVcode1";
+            this.picVcode1.Size = new System.Drawing.Size(139, 50);
+            this.picVcode1.TabIndex = 2;
+            this.picVcode1.TabStop = false;
+            // 
+            // btnRefresh2
+            // 
+            this.btnRefresh2.Location = new System.Drawing.Point(276, 199);
+            this.btnRefresh2.Name = "btnRefresh2";
+            this.btnRefresh2.Size = new System.Drawing.Size(104, 50);
+            this.btnRefresh2.TabIndex = 5;
+            this.btnRefresh2.Text = "刷新";
+            this.btnRefresh2.UseVisualStyleBackColor = true;
+            this.btnRefresh2.Click += new System.EventHandler(this.btnRefresh2_Click);
+            // 
+            // picVcode2
+            // 
+            this.picVcode2.BackColor = System.Drawing.Color.White;
+            this.picVcode2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picVcode2.Location = new System.Drawing.Point(120, 199);
+            this.picVcode2.Name = "picVcode2";
+            this.picVcode2.Size = new System.Drawing.Size(139, 50);
+            this.picVcode2.TabIndex = 4;
+            this.picVcode2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "数字验证码：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "数字字母验证码：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "汉字验证码：";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(197, 286);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(119, 12);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "happynetlife@qq.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "2016-01-24  点石网络 王兴民";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 310);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRefresh2);
+            this.Controls.Add(this.picVcode2);
+            this.Controls.Add(this.btnRefresh1);
+            this.Controls.Add(this.picVcode1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.picVcode);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "验证码生成";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picVcode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVcode1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVcode2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picVcode;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnRefresh1;
+        private System.Windows.Forms.PictureBox picVcode1;
+        private System.Windows.Forms.Button btnRefresh2;
+        private System.Windows.Forms.PictureBox picVcode2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CAPTCHA-demo1/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAPTCHA_demo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picVcode.Image = createCaptcha(rndNumber(5));
+            picVcode1.Image = createCaptcha(rndStr(5));
+            picVcode2.Image = createCaptchaCN(getRndStrGB(4));
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            picVcode.Image = createCaptcha(rndNumber(5));
+        }
+
+        private void btnRefresh1_Click(object sender, EventArgs e)
+        {
+            picVcode1.Image = createCaptcha(rndStr(5));
+        }
+
+        private void btnRefresh2_Click(object sender, EventArgs e)
+        {
+            string str = getRndStrGB(4);
+            Bitmap image = createCaptchaCN(str);
+            if (image != null)
+            {
+                picVcode2.Image = image;
+            }
+        }
+
+
+        /// <summary>
+        /// 生成一个随机字串
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        private string rndStr(int len)
+        {
+            // 随机数对象
+            Random rnd = new Random();
+            // 组成字符
+            string chars = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string str = null;
+            for(int i = 0; i < len; i++)
+            {
+                str += chars.Substring(rnd.Next() % chars.Length, 1);
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 生成一个随数字组成的字符串
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        private string rndNumber(int len)
+        {
+            // 创建一个随机数对象
+            Random rnd = new Random();
+            // 产生一个随机数
+            string str = null;
+            int rndNum = 0;
+            for (int i = 0; i < len; i++)
+            {
+                rndNum = rnd.Next(0, 10);
+                str += rndNum;
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 生成验证码图片
+        /// </summary>
+        private Bitmap createCaptcha(string str)
+        {
+            // 创建一个随机数对象
+            Random rnd = new Random();
+            int strLen = str.Length;
+            // 创建一个位图对象
+            int imgWidth = 139;
+            int imgHeight = 50;
+            Bitmap bmp = new Bitmap(imgWidth, imgHeight);
+            // 创建一个GDI对象
+            Graphics g = Graphics.FromImage(bmp);
+            // 坐标点
+            Point p1, p2;
+            // 绘制干扰点
+            for (int i = 0; i < 500; i++)
+            {
+                p1 = new Point(rnd.Next(0, imgWidth), rnd.Next(0, imgHeight));
+                // 画点
+                bmp.SetPixel(p1.X, p1.Y, Color.Black);
+            }
+            // 绘制干扰线
+            for (int i = 0; i < 20; i++)
+            {
+                p1 = new Point(rnd.Next(0, imgWidth), rnd.Next(0, imgHeight));
+                p2 = new Point(rnd.Next(0, imgWidth), rnd.Next(0, imgHeight));
+                // 绘制线条
+                g.DrawLine(new Pen(Brushes.Gray), p1, p2);
+            }
+            // 字体数组
+            string[] fonts = { "宋体","微软雅黑","黑体","Consolas","楷体","仿宋","幼圆","隶书"};
+            int fontLen = fonts.Length;
+            // 颜色数组
+            Color[] colors = { Color.Yellow, Color.Red, Color.Blue, Color.Green, Color.Pink, Color.Teal, Color.Aqua };
+            int colorLen = colors.Length;
+            // 字体字号
+            Font font;
+
+            for(int i = 0; i < strLen; i++)
+            {
+                p1 = new Point(10 + i * 20, rnd.Next(5, 15));
+                font = new Font(fonts[rnd.Next(0, fontLen)], rnd.Next(16, 24), FontStyle.Bold);
+                // 画字符
+                g.DrawString(str[i].ToString(), font, new SolidBrush(colors[rnd.Next(0, colorLen)]), p1);
+            }
+            
+            // 返加位图对象
+            return bmp;
+        }
+
+
+        /// <summary>
+        /// 生成随机汉字数组
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static object[] CreateCode(int length)
+        {
+            // 定义字符串存储汉字编码元素
+            string[] r = new string[16] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+            // 创建随机对象
+            Random rnd = new Random();
+            object[] bytes = new object[length];
+            // 循环产生字节数组
+            int r1, r2, r3, r4;
+            string str1, str2, str3, str4;
+            byte byte1, byte2;
+            byte[] strR;
+
+            for (int i = 0; i < length; i++)
+            {
+                // 区位码第一位
+                r1 = rnd.Next(11, 14);
+                str1 = r[r1].Trim();
+                // 避免产生相同的字
+                rnd = new Random(r1 * unchecked((int)DateTime.Now.Ticks) + i);
+
+                // 区位码第二位
+                if(r1 == 13)
+                {
+                    r2 = rnd.Next(0, 7);
+                }
+                else
+                {
+                    r2 = rnd.Next(0, 16);
+                }
+                str2 = r[r2];
+                // 避免重复
+                rnd = new Random(r2 * unchecked((int)DateTime.Now.Ticks) + i);
+
+                // 区位码第三位
+                r3 = rnd.Next(10, 16);
+                str3 = r[r3];
+
+                // 避免重复
+                rnd = new Random(r3 * unchecked((int)DateTime.Now.Ticks) + i);
+
+                // 区位码第四位
+                if(r3 == 10)
+                {
+                    r4 = rnd.Next(1, 16);
+                }
+                else if(r3 == 15)
+                {
+                    r4 = rnd.Next(0, 15);
+                }
+                else
+                {
+                    r4 = rnd.Next(0, 16);
+                }
+                str4 = r[r4];
+                // 字义两个变量存储产生的随机汉字区位码
+                byte1 = Convert.ToByte(str1 + str2, 16);
+                byte2 = Convert.ToByte(str3 + str4, 16);
+                // 将临时变量的汉字区位码存储到字节数组
+                strR = new byte[] { byte1, byte2 };
+                // 将汉字存储到object数组
+                bytes.SetValue(strR, i);
+            }
+            return bytes;
+        }
+
+        /// <summary>
+        /// 生成随机的汉字字串
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private string getRndStrGB(int length)
+        {
+            // 获取gb2312编码页
+            Encoding gb = Encoding.GetEncoding("gb2312");
+            // 调用产生的随机汉字编码
+            object[] bytes = CreateCode(length);
+            // 解码汉字
+            string str = null;
+            for (int i = 0; i < length; i++)
+            {
+                str += gb.GetString((byte[])Convert.ChangeType(bytes[i], typeof(byte[])));
+            }
+            // 返回汉字字串
+            return str;
+        }
+
+        /// <summary>
+        /// 生成中文验证码图片
+        /// </summary>
+        /// <param name="str"></param>
+        public Bitmap createCaptchaCN(string str)
+        {
+            // 判断字串不为空
+            if (string.IsNullOrEmpty(str))
+            {
+                return null;
+            }
+            int strLen = str.Length;
+            // 创建位图对象
+            int imgWidth = (int)Math.Ceiling(strLen * 32.5);
+            int imgHeight = 50;
+            Bitmap bmp = new Bitmap(imgWidth, imgHeight);
+            // 创建绘图对象
+            Graphics g = Graphics.FromImage(bmp);
+            // 创建随机对象
+            Random rnd = new Random();
+            // 字体数组
+            string[] fonts = { "宋体", "微软雅黑", "黑体", "楷体", "仿宋", "幼圆", "隶书" };
+            int fontCount = fonts.Length;
+            // 颜色数组
+            Color[] colors = { Color.YellowGreen, Color.Red, Color.Blue, Color.Green, Color.DarkCyan, Color.Teal, Color.Black };
+            int colorCount = colors.Length;
+
+            try
+            {
+                // 清空区域
+                g.Clear(Color.White);
+                // 坐标变量
+                Point p1, p2;
+                // 画背景线
+                for (int i = 0; i < 20; i++)
+                {
+                    p1 = new Point(rnd.Next(imgWidth), rnd.Next(imgHeight));
+                    p2 = new Point(rnd.Next(imgWidth), rnd.Next(imgHeight));
+                    // 绘制线条
+                    //g.DrawLine(new Pen(Color.FromArgb(rnd.Next())), p1, p2);
+                    //g.DrawLine(new Pen(colors[rnd.Next(0, colorCount)]), p1, p2);//随机颜色
+                    g.DrawLine(new Pen(Color.Gray), p1, p2);
+                }
+                // 画干扰点
+                for (int i = 0; i < 500; i++)
+                {
+                    p1 = new Point(rnd.Next(imgWidth), rnd.Next(imgHeight));
+                    bmp.SetPixel(p1.X, p1.Y, Color.Black);
+                }
+                // 画汉字
+                Font font;
+                // 线性渐变笔刷对象
+                LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, imgWidth, imgHeight), colors[rnd.Next(0, colorCount)], colors[rnd.Next(0, colorCount)], 1.2f, true);
+                // 一个一个绘制
+                for (int i = 0; i < strLen; i++)
+                {
+                    // 定义字体字号
+                    font = new Font(fonts[rnd.Next(0, fontCount)], rnd.Next(16, 20), FontStyle.Bold);
+                    // 绘制汉字
+                    g.DrawString(str[i].ToString(), font, brush, 10 + i * 25, rnd.Next(5, 15));
+                }
+                
+                // 绘制边框
+                //g.DrawRectangle(new Pen(Color.Silver), 0, 0, imgWidth - 1, imgHeight - 1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                g.Dispose();
+            }
+            // 返回位图
+            return bmp;
+        }
+    }
+}
